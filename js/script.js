77938d244828ce0e35d963197e3b05ced2b0cb93
Marvyn Gulina <marvyn.gulina@unamur.be>
2026-02-27 20:11:55 +0100

    renamed upgrade into building and added buildings

--- js/script.js
+++ js/script.js
@@ -14,34 +14,55 @@ const counterPerClick = document.getElementById("counterPerClick");
 let bakerTitle = document.getElementById("bakerTitle");
 if (cookies == 0) bakerTitle.textContent = "apprentice baker";
 
-// Améliorations
-let upgradesList = [
-	{ name: "Electric Oven", factor: 1, price: 20 },
-	{ name: "Fancy Grandma", factor: 2, price: 50 },
-	{ name: "Farm", factor: 4, price: 150 },
-	{ name: "Mine", factor: 8, price: 400 },
-	{ name: "Factory", factor: 16, price: 1000 },
-	{ name: "Bank", factor: 32, price: 2500 },
+// Liste des batiments
+let buildingList = [
+	{ name: "Electric Oven", cpc: 1, price: 10*2, quantity: 0 },
+	{ name: "Fancy Grandma", cpc: 2, price: 10*(2**2), quantity: 0 },
+	{ name: "Farm", cpc: 2 ** 2, price: 10*(2**3), quantity: 0 },
+	{ name: "Mine", cpc: 2 ** 3, price: 10*(2**4), quantity: 0 },
+	{ name: "Factory", cpc: 2 ** 4, price: 10*(2**5), quantity: 0 },
+	{ name: "Bank", cpc: 2 ** 5, price: 10*(2**6), quantity: 0 },
+	{ name: "Temple", cpc: 2 ** 6, price: 10*(2**7), quantity: 0 },
+	{ name: "Wizard tower", cpc: 2 ** 7, price: 10*(2**8), quantity: 0 },
+	{ name: "Shipment", cpc: 2 ** 8, price: 10*(2**9), quantity: 0 },
+	{ name: "Alchemy lab", cpc: 2 ** 9, price: 10*(2**10), quantity: 0 },
+	{ name: "Portal", cpc: 2 ** 10, price: 10*(2**11), quantity: 0 },
+	{ name: "Time machine", cpc: 2 ** 11, price: 10*(2**12), quantity: 0 },
+	{ name: "Antimatter condenser", cpc: 2 ** 12, price: 10*(2**13), quantity: 0 },
+	{ name: "Prism", cpc: 2 ** 13, price: 10*(2**14), quantity: 0 },
+	{ name: "Chancemaker", cpc: 2 ** 14, price: 10*(2**15), quantity: 0 },
+	{ name: "Fractal engine", cpc: 2 ** 15, price: 10*(2**16), quantity: 0 },
+	{ name: "Javascript console", cpc: 2 ** 16, price: 10*(2**17), quantity: 0 },
+	{ name: "Idleverses", cpc: 2 ** 17, price: 10*(2**18), quantity: 0 },
+	{ name: "Cortex baker", cpc: 2 ** 18, price: 10*(2**19), quantity: 0 },
+	{ name: "You", cpc: 2 ** 19, price: 10*(2**20), quantity: 0 }
 	];
 
-	let upgradeBlock = document.getElementById("upgradeBlock");
-	for (let upgrade of upgradesList) {
+	let buildingBlock = document.getElementById("buildingBlock");
+	for (let building of buildingList) {
 		let li = document.createElement("li");
-		let upgradeButton = document.createElement("button");
-		upgradeButton.textContent = upgrade.name + " (+" + upgrade.factor + "),	cost: " + upgrade.price + " cookies";
-		upgradeButton.addEventListener("click", function() {
-			buyUpgrade(upgrade);
+		let buildingButton = document.createElement("button");
+		buildingButton.textContent = building.name + " (+" + building.cpc + " cpc),	cost: " + building.price + " cookies" + " (owned: " + building.quantity + ")";
+		buildingButton.addEventListener("click", function() {
+			buyBuilding(building);
 		});
-		li.appendChild(upgradeButton);
-		upgradeBlock.appendChild(li);
+		li.appendChild(buildingButton);
+		buildingBlock.appendChild(li);
 		}
 
-	function buyUpgrade(upgrade) {
-		if (cookies >= upgrade.price) {
-			cookies = cookies - upgrade.price;
-			cookiesPerClick = cookiesPerClick + upgrade.factor;	
+	function buyBuilding(building) {
+		if (cookies >= building.price) {
+			cookies = cookies - building.price;
+			cookiesPerClick = cookiesPerClick + building.cpc;	
 			counter.textContent = cookies;
 			counterPerClick.textContent = cookiesPerClick;
+			building.quantity++;
+
+			// Mise à jour du nombre d'améliorations achetées
+			let buildingButtons = buildingBlock.getElementsByTagName("button");
+			for (let i = 0; i < buildingList.length; i++) {
+				buildingButtons[i].textContent = buildingList[i].name + " (+" + buildingList[i].cpc + " cpc),	cost: " + buildingList[i].price + " cookies" + " (owned: " + buildingList[i].quantity + ")";
+			}
 		}
 	}
 
@@ -65,7 +86,5 @@ mainButton.addEventListener("click", function() {
 	}
 	else if (cookiesLegacy >= 10) {
 		bakerTitle.textContent = "junior baker";
-	}
-	
-	
+	}	
 	});
